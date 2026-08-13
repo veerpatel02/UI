487577ef4c7852932087ccc5e3ab46cc5a692897
veer patel <veerpatel02007@gmail.com>
2026-08-13 23:38:49 +0530

    remove comment

--- src/components/Section1/RightContent.jsx
+++ src/components/Section1/RightContent.jsx
@@ -11,25 +11,4 @@ const RightContent = (props) => {
     </div>
   )
 }
-
-
-// import React from 'react'
-// import RightCardContent from './RightCardContent'
-
-// const RightCard = (props) => {
-//   return (
-//     <div className='h-full overflow-hidden shrink-0 relative w-80 rounded-4xl'>
-//       <img
-//         className='h-full w-full object-cover'
-//         src={props.img}
-//         alt=""
-//       />
-
-//       <RightCardContent />
-//     </div>
-//   )
-// }
-
-
-
 export default RightContent
